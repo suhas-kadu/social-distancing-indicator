fbf225a622de2d476fd7e537f52f23a99ff4d006
suhas-kadu <kadusuhas72@gmail.com>
2021-02-28 06:08:12 +0530

    ultrasonic sensor assembled

--- social_distancing_indicator.ino
+++ social_distancing_indicator.ino
@@ -1,9 +1,32 @@
+
+int echo = 8;
+int trig = 9;
+long duration = 0;
+int distance = 0;
+
 void setup()
 {
  // put your setup code here, to run once:
+	pinMode(echo, INPUT);		// setting trigger pin to output
+	pinMode(trig, OUTPUT);	// setting echo pin to input
+	Serial.begin(4800);		// giving the baud rate for serial port
+
 }
 
 void loop()
 {
    // put your main code here, to run repeatedly:
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  
+  // generating a 10 microseconds pulse
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  
+  
+  duration = pulseIn(echo, HIGH);	// measuring the duration of pulse from ECHO pin
+  distance  = (duration*0.034)/2) + 4; 	// speed of sound in cm/microseconds
+  Serial.print("Distance: ");
+  Serial.println(distance);		
 }
