a6e2d8e8e7fc39ad4220516e4104ce2083aed2c9
suhas-kadu <kadusuhas72@gmail.com>
2021-02-28 12:34:34 +0530

    social distancing conditions added, RGB LED added

--- social_distancing_indicator.ino
+++ social_distancing_indicator.ino
@@ -1,4 +1,7 @@
 
+int greenPin = 5;
+int bluePin = 6;
+int redPin = 7;
 int echo = 8;
 int trig = 9;
 long duration = 0;
@@ -6,10 +9,13 @@ int distance = 0;
 
 void setup()
 {
- // put your setup code here, to run once:
-	pinMode(echo, INPUT);		// setting trigger pin to output
-	pinMode(trig, OUTPUT);	// setting echo pin to input
-	Serial.begin(4800);		// giving the baud rate for serial port
+	// put your setup code here, to run once:
+	pinMode(echo, INPUT);			// setting trigger pin to output
+	pinMode(trig, OUTPUT);			// setting echo pin to input
+	pinMode(greenPin, OUTPUT);		// setting green pin of RGB to output
+	pinMode(bluePin, OUTPUT);		// setting blue pin of RGB to output
+	pinMode(redPin, OUTPUT);		// setting red pin of RGB to output
+	Serial.begin(4800);				// giving the baud rate for serial port
 
 }
 
@@ -25,8 +31,30 @@ void loop()
   digitalWrite(trig, LOW);
   
   
-  duration = pulseIn(echo, HIGH);	// measuring the duration of pulse from ECHO pin
-  distance  = (duration*0.034)/2) + 4; 	// speed of sound in cm/microseconds
+  duration = pulseIn(echo, HIGH);						// measuring the duration of pulse from ECHO pin
+  distance  = (duration*0.034)/2) + 4; 					// speed of sound in cm/microseconds
   Serial.print("Distance: ");
-  Serial.println(distance);		
+  Serial.println(distance);	
+  
+  
+  if(distance > 0 && distance <= 180){					// if some one does'nt maintains social distance					
+  Serial.println("Alert: Social distance not maintained!");
+   	// if social distance is not maintained red color is set to RGB LED
+    analogWrite(redPin, 138);
+    analogWrite(greenPin, 0);
+    analogWrite(bluePin, 0);
+    
+    
+  } else if(distance > 180 && distance <= 300){	 		// if social distance is maintained
+  Serial.println("Safe: Social distance maintained!");  
+ 	// if social distance is maintained green color is set to RGB LED
+  	analogWrite(redPin, 0);
+    analogWrite(greenPin, 255);
+    analogWrite(bluePin, 0);
+  } else {												// if no one is around us
+  Serial.println("Safe");
+    // 	if person is out of range no color is et to RGB LED
+    analogWrite(redPin, 0);
+    analogWrite(greenPin, 0);
+    analogWrite(bluePin, 0);
 }
